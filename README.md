a071f5a66b87e88f5baeae8a5edf093c0868637c
jonyrock <ilms@live.ru>
2013-08-05 01:00:38 +0400

    matplotlib plotting

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ Bishop
 ======
 
 My study tools. Something about linear algebra, machine learning and algorithms in python. 
+
+
+Install
+=======
+Use packages:
+* python3-matplotlib
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 
-from plotting import plot
+import matplotlib.pyplot as plt
 
 data = list(open('datasets/curvefitting.txt'))
 dots = list([tuple(map(float, x.strip().split(' '))) for x in data])
-
-print(dots)
-plot(dots, 1)
+xdots = [x[0] for x in dots]
+ydots = [x[1] for x in dots]
 
 # print(dots)
+plt.plot(xdots, ydots, 'ko')
+plt.axis([-0.1, 1.1, -1.3, 1.3])
+plt.show()
